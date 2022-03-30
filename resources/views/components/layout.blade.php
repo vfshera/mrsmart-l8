d0bf53dbb30ae085142f5484fea34ce433ca1880
Franklin Shera <fshera96@gmail.com>
2022-03-31 02:55:16 +0300

    Local Fonts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,9 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
-
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -15,9 +12,6 @@
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#fefefe">
     <meta name="theme-color" content="#ffffff">
-
-
-
     <title>{{ config('app.name', 'MrSmart') }}</title>
     <meta name="description" content="MrSmart Cleaning Services">
     <meta name="keywords" content="Upholstery Cleaning , Carpet Cleaning , Dry Cleaning , Car Interior Cleaning">
