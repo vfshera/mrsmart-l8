1698996dcfb95fbf07df505c2bf3e90854fb6268
Franklin Shera <fshera96@gmail.com>
2022-04-04 18:27:29 +0300

    Test:contact mail sent queued

--- tests/Feature/Mail/ContactSentMailTest.php
+++ tests/Feature/Mail/ContactSentMailTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature\Mail;
 
+use App\Http\Livewire\ContactForm;
 use App\Mail\ContactSentMail;
 use App\Models\ContactMessage;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class ContactSentMailTest extends TestCase
@@ -12,7 +15,7 @@ class ContactSentMailTest extends TestCase
     use RefreshDatabase;
 
     /** @test **/
-    public function contact_mail_content()
+    public function contact_mail_sent_content()
     {
         $contact = ContactMessage::factory()->create();
 
@@ -22,6 +25,22 @@ class ContactSentMailTest extends TestCase
 
         $mailable->assertSeeInHtml($contact->name);
         $mailable->assertSeeInHtml("Hello");
+    }
+
+    /** @test **/
+    public function contact_mail_sent_queued()
+    {
+        Mail::fake();
+
+        Livewire::test(ContactForm::class)
+            ->set('name', 'Sender Name')
+            ->set('email', 'sender@examplmail.com')
+            ->set('message', 'Something useful')
+            ->call('submit')
+            ->assertRedirect('/');
+
+        Mail::assertQueued(ContactSentMail::class);
+
     }
 
 }
